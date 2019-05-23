361196828f0db4d0729c8905f104a5f7a4319e39
huyichen <huyichen@camelotchina.com>
2019-05-23 19:12:20 +0800

    代码提交

--- build/constants.js
+++ build/constants.js
@@ -1,6 +1,8 @@
 const APP_ENV = process.env.NODE_ENV || 'prod'
 const FILE_EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx']
 
+console.log(process.env)
+
 module.exports = {
     APP_ENV,
     FILE_EXTENSIONS
--- build/plugins.js
+++ build/plugins.js
@@ -1,22 +1,65 @@
+const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 
-const { resolveAssetsRootDir } = require('./utils')
+const { assetsPath } = require('./utils')
 
-module.exports = [
+const constants = require('./constants')
+const env = require('./env.json')
+
+const oriEnv = env[constants.APP_ENV]
+Object.assign(oriEnv, {
+    APP_ENV: constants.APP_ENV
+})
+
+const defineEnv = {}
+for (let key in oriEnv) {
+    defineEnv[`process.env.${key}`] = JSON.stringify(oriEnv[key])
+}
+const DLL_PATH = '../dll'
+
+const basePlugins = [
+  new webpack.DefinePlugin(defineEnv)
+]
+
+const devPlugins = [
   new HtmlWebpackPlugin({
-    // 以哪个文件为模板，模板路径
-    template: "public/index.html",
-    // 编译后的文件名
-    filename: "index.html",
-    inject: true,
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true,
-      removeAttributeQuotes: true
-    }
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true
+  }),
+]
+
+const prodPlugins = [
+  new webpack.DllReferencePlugin({
+      manifest: require(`${DLL_PATH}/vendor.manifest.json`)
+  }),
+  new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true,
+      minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+          // more options:
+          // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+  }),
+  new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, `${DLL_PATH}/**/*.js`),
+      includeSourcemap: false
   }),
   new MiniCssExtractPlugin({
-    filename: resolveAssetsRootDir('css/[name].css')
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: assetsPath('css/[name].[contenthash].css'),
+      chunkFilename: assetsPath('css/[name].[id].[contenthash].css')
   })
 ]
+
+module.exports = basePlugins.concat(constants.APP_ENV === 'dev' ? devPlugins : prodPlugins)
