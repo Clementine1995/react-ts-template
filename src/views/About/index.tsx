8a477ac1a03602c228a967b6a458179318b91a54
huyichen <v-huyichen.ea@inc.bytedance.com>
2019-06-20 11:23:19 +0800

    增加一个hook使用小例子

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
 
+import Test2 from '@components/Test2'
+
 class About extends React.Component{
   render () {
     return (
-      <div>About</div>
+      <div>
+        <p>About</p>
+        <Test2 />
+      </div>
     )
   }
 }
