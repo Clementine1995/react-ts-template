aabea3215e39945377bdc451a1d4e324ff6c0a67
huyichen <v-huyichen.ea@inc.bytedance.com>
2019-06-21 19:35:07 +0800

    增加一些hook使用例子

--- src/components/Test2/index.tsx
+++ src/components/Test2/index.tsx
@@ -1,22 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function Test2() {
   const [count, setCount] = useState(0)
-
   // 相当于 componentDidMount 和 componentDidUpdate:
   useEffect(() => {
     // 使用浏览器的 API 更新页面标题
     document.title = `You clicked ${count} times`
   })
+  const prevCount = usePrevious(count)
 
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert('You clicked on: ' + count)
+    }, 3000)
+  }
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You clicked {count} times , before: {prevCount}</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <button onClick={handleAlertClick}>
+        Show alert
+      </button>
+      <br/>
+      {/* <Counter /> */}
     </div>
   )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(c => c + 1) // 这个 effect 依赖于 `count` state
+    }, 1000)
+    return () => clearInterval(id)
+  }, []) // 🔴 Bug: `count` 没有被指定为依赖
+
+  return <h1>{count}</h1>
+}
+
+function usePrevious(value: any) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
 }
 
 export default Test2
