358757ad765327a35f0da5627f5f3ce219736dbc
yichen <18669859508@163.com>
2019-12-16 23:10:41 +0800

    :art: 更新eslint配置，有待优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     "prettier",
+    "prettier/@typescript-eslint",
     'plugin:prettier/recommended'
   ],
   parserOptions: {
