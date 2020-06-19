f1cf6fcf275e1a87236a263adb0cb519debe20f1
Clementine1995 <18669859508@163.com>
2020-06-19 17:34:51 +0800

    :wrench: 调整配置文件

--- build2/webpack.common.js
+++ build2/webpack.common.js
@@ -91,8 +91,11 @@ module.exports = {
           {
             loader: 'less-loader',
             options: {
-              javascriptEnabled: true,
-              modifyVars: theme
+              lessOptions: {
+                // 如果使用less-loader@5，请移除 lessOptions 这一级直接配置选项。
+                modifyVars: theme,
+                javascriptEnabled: true
+              }
             }
           }
         ]
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import 'antd/dist/antd.css'
+// import 'antd/dist/antd.css'
+import 'antd/dist/antd.less'
 import './styles/reset.css'
 import './index.scss'
 
